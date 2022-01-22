24f2287cdbc2d81463b5f1008856d8e82feb7c80
eiriktaa <eirikamdal@yahoo.com>
2022-01-22 14:21:06 +0100

    added config options

--- ssh-keys-to-ifi.bash
+++ ssh-keys-to-ifi.bash
@@ -18,7 +18,6 @@ echo "   HostName login.ifi.uio.no" >> ~/.ssh/config
 echo "   User $username" >> ~/.ssh/config
 echo "   IdentityFile ~/.ssh/ifi-login" >> ~/.ssh/config
 echo "   " >> ~/.ssh/config
-=======
 
 #tests connection
 ssh ${username}@login.ifi.uio.no
